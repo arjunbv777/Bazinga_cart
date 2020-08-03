a956f9432a1380e9a0cec44e98ae86ef2fbaf65f
varadana <arjun.varadannavar@mitel.com>
2020-08-03 13:31:48 +0530

    init configration of h2 and log configration

---
+++ src/main/java/com/bazinga/BazingaCartApplication.java
@@ -0,0 +1,13 @@
+package com.bazinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazingaCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazingaCartApplication.class, args);
+	}
+
+}
